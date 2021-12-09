fda903066b73a406bbe41b2bcc3d211506be89d1
Azu <abbey-1825@hotmail.com>
2021-12-09 14:35:04 -0300

    install linter, prettier and styled-components, and create initial style constants

---
+++ src/styles/constants.js
@@ -0,0 +1,12 @@
+const COLORS = {
+  SOFT_BLUE: 'hsl(231, 69%, 60%)',
+  SOFT_RED: 'hsl(0, 94%, 66%)',
+  GREYISH_BLUE: 'hsl(229, 8%, 60%)',
+  DARK_BLUE: 'hsl(229, 31%, 21%)',
+}
+
+const FONT_SIZES = {
+  DEFAULT: '18px',
+}
+
+export { COLORS, FONT_SIZES }
