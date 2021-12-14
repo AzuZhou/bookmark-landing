600ab5911091423d61cbf9751f106a57f1659bc2
Azu <abbey-1825@hotmail.com>
2021-12-14 05:27:39 -0300

    handle Newsletter email validation and errors

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,13 +1,32 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 import { ContentContext } from 'context/ContentContext';
 
-import { Section, Submit } from './styled';
+import ErrorIcon from '../../../public/icon-error.svg';
+
+import { validateEmail } from 'utils/helpers';
+
+import { Section, Form, Input, Submit, ErrorMessage } from './styled';
 
 const Newsletter = () => {
   const { page } = useContext(ContentContext);
+  const [email, setEmail] = useState('');
+  const [isError, setIsError] = useState(false);
   const { newsletter } = page || {};
   const { ctaText, inputPlaceholder, tagline, title } = newsletter;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!email) return setIsError(true);
+
+    if (validateEmail(email)) {
+      setIsError(false);
+      setEmail('');
+    } else {
+      setIsError(true);
+    }
+  };
 
   return (
     <Section>
@@ -16,10 +35,25 @@ const Newsletter = () => {
 
         <h2>{title}</h2>
 
-        <form>
-          <input type="email" placeholder={inputPlaceholder} />
+        <Form onSubmit={handleSubmit} isError={isError} noValidate>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Input
+              id="email"
+              name="email"
+              value={email}
+              isError={isError}
+              type="email"
+              placeholder={inputPlaceholder}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => isError && setIsError(false)}
+            />
+            <ErrorIcon />
+            <ErrorMessage isError={isError}>{`Whoops, make sure it's an email`}</ErrorMessage>
+          </div>
+
           <Submit type="submit">{ctaText}</Submit>
-        </form>
+        </Form>
       </div>
     </Section>
   );
--- src/components/Newsletter/styled.js
+++ src/components/Newsletter/styled.js
@@ -18,7 +18,7 @@ const Section = styled.section`
     padding: 60px ${PADDING.MOBILE};
     margin: 0 auto;
 
-    span {
+    > span {
       color: ${COLORS.WHITE};
       text-transform: uppercase;
       letter-spacing: 4px;
@@ -29,24 +29,6 @@ const Section = styled.section`
       color: ${COLORS.WHITE};
       text-align: center;
       margin: 12px 0 30px 0;
-    }
-
-    form {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 16px;
-
-      input {
-        width: 100%;
-        padding: 12px 16px;
-
-        border-radius: 4px;
-        border: 2px solid transparent;
-        font-size: ${FONT_SIZES.MOBILE.PRIMARY_TEXT};
-        line-height: 26px;
-      }
     }
   }
 
@@ -65,19 +47,93 @@ const Section = styled.section`
         margin: 0;
         max-width: 500px;
       }
-
-      form {
-        flex-direction: row;
-        justify-content: center;
-
-        input {
-          max-width: 300px;
-          font-size: ${FONT_SIZES.DESKTOP.PRIMARY_TEXT};
-          line-height: 24px;
-        }
-      }
     }
   }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+  > div {
+    position: relative;
+    width: 100%;
+    overflow: ${({ isError }) => (isError ? 'visible' : 'hidden')};
+
+    label {
+      height: 1px;
+      position: absolute;
+      left: -10000px;
+    }
+
+    svg {
+      position: absolute;
+      z-index: 2;
+      right: 12px;
+      top: 17px;
+
+      transition: opacity 0.2s linear;
+      opacity: ${({ isError }) => (isError ? '1' : '0')};
+    }
+  }
+
+  ${desktopBreakpoint} {
+    flex-direction: row;
+    justify-content: center;
+
+    > div {
+      height: 54px;
+      max-width: 300px;
+    }
+  }
+`;
+
+const Input = styled.input`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  padding: 12px 16px;
+
+  border-radius: 4px;
+  border: 2px solid transparent;
+  font-size: ${FONT_SIZES.MOBILE.PRIMARY_TEXT};
+  line-height: 26px;
+  border-color: ${({ isError }) => (isError ? COLORS.SOFT_RED : 'transparent')};
+
+  ${desktopBreakpoint} {
+    font-size: ${FONT_SIZES.DESKTOP.PRIMARY_TEXT};
+    line-height: 24px;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  width: 100%;
+
+  font-size: ${FONT_SIZES.MOBILE.SECONDARY_TEXT};
+  font-style: italic;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.SOFT_RED};
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+  transition: height 0.2s linear, transform 0.2s linear, opacity 0.2s linear;
+
+  ${({ isError }) =>
+    isError
+      ? `
+  height: auto;
+  transform: translateY(-2px);
+  opacity: 1;
+  padding: 6px 12px;
+  `
+      : `
+  height: 0;
+  transform: translateY(-100%);
+  opacity: 0;
+  padding: 0;
+  `}
 `;
 
 const Submit = styled.button`
@@ -91,4 +147,4 @@ const Submit = styled.button`
   }
 `;
 
-export { Section, Submit };
+export { Section, Submit, Form, Input, ErrorMessage };
