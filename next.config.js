d668dc4f8625dda3de0fe501dd2a390dbc8ffc93
Azu <abbey-1825@hotmail.com>
2021-12-15 18:45:10 -0300

    remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching
-  },
+module.exports = {
   images: {
     domains: ['images.ctfassets.net']
   }
-});
+};
