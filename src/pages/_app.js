5061dc76c63c34eae29d78e056571f7db4a0e32c
Azu <abbey-1825@hotmail.com>
2021-12-15 18:00:30 -0300

    change charset for charSet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="hsl(231, 69%, 60%)" />
         <link rel="icon" href="/favicon.png" />
