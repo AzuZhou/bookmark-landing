de6745cc831b4c15b89a971d08a5aebf0d4dfe50
Azu <abbey-1825@hotmail.com>
2021-12-15 17:55:42 -0300

    remove unused libraries and fix _document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,26 @@
+import Head from 'next/head';
+
 import GlobalStyle from 'styles/globalStyle';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="hsl(231, 69%, 60%)" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="Bookmark Landing" />
+        <title>Bookmark</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,32 +25,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="hsl(231, 69%, 60%)" />
-          <link rel="icon" href="/favicon.png" />
-          <link rel="apple-touch-icon" href="/logo192.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="description" content="Bookmark Landing" />
-          <title>Bookmark</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
